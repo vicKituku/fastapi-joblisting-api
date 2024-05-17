e10202d43aeabaf7c9a98e4e87584e1275ec48c9
VicKituku <victorkituku@gmail.com>
2024-05-17 12:22:49 +0300

    configure CORS for the react application to access the data

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ from typing import Annotated, List
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+origins = [
+    'https://react-tailwindcss-job-listing-website.vercel.app/',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers=["*"]
+
+)
 
 class JobListingBase(BaseModel):
     title: str
